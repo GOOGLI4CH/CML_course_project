3538da36d677de776c681fc5c144a6bc1d2df53d
GOOGLICH <Uglyanitsa_M@it.belstu.by>
2023-05-01 18:10:52 +0300

    add new sliders

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-let swiper = new Swiper(".swiper-container", {
+let mainSwiper = new Swiper(".swiper-container", {
     slidesPerView: 3,
     spaceBetween: 30,
     freeMode: true,
@@ -7,7 +7,7 @@ let swiper = new Swiper(".swiper-container", {
     grabCursor: true,
     keyboard: {
         enabled: true,
-      },
+    },
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -16,5 +16,34 @@ let swiper = new Swiper(".swiper-container", {
         el: ".swiper-pagination",
         dynamicBullets: true,
         clickable: true,
+    },
+});
+
+let swiper = new Swiper(".scroll-container", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    autoHeight: true,
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    mousewheel: true,
+  });
+
+let aboutSwiper = new Swiper(".halls-swiper", {
+    freeMode: true,
+    loop: true,
+    effect: "fade",
+    keyboard: {
+        enabled: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
 });
