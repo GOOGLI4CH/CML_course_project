98b291a6e6eab5307cfeb8c06f581cd7c7d4ead8
GOOGLICH <Uglyanitsa_M@it.belstu.by>
2023-05-01 04:16:57 +0300

    swiper debugging

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,13 @@
 let swiper = new Swiper(".swiper-container", {
     slidesPerView: 3,
-    spaceBetween: 15,
+    spaceBetween: 30,
     freeMode: true,
+    loop: true,
     mousewheel: true,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+      },
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
