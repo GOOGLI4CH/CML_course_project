96d3f4529a8404366af5419d88b8937d9cc33eb7
GOOGLICH <Uglyanitsa_M@it.belstu.by>
2023-05-07 03:30:34 +0300

    debugging

--- js/popup.js
+++ js/popup.js
@@ -7,9 +7,9 @@ export function ModalWindow() {
         const popup = document.querySelector('.popup');
         popupLinks.forEach((popupLinks) => {
             popupLinks.addEventListener('click', (e) => {
+                e.preventDefault();
                 popup.classList.add('active');
                 document.body.classList.add('lock');
-                e.preventDefault();
                 active = true;
             });
         });
@@ -18,9 +18,9 @@ export function ModalWindow() {
     if (active) {
         const close = document.querySelector('.popupClose');
         close.addEventListener('click', (e) => {
+            e.preventDefault();
             popup.classList.remove('active');
             document.body.classList.remove('lock');
-            e.preventDefault();
             active = false;
         });
     }
